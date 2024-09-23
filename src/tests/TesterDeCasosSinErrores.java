54f0e97c3b2a4c9eda078e37c211482edf2d983b
Valentina Navarro <71287615+ValenAle612@users.noreply.github.com>
2024-09-23 11:03:31 -0300

    cositas

--- src/tests/TesterDeCasosSinErrores.java
+++ src/tests/TesterDeCasosSinErrores.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
+import Main_Controller.Main;
 import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Test;
@@ -22,7 +23,7 @@ public class TesterDeCasosSinErrores {
     private static final String msgExito = "[SinErrores]";
     private static final String testFilesDirectoryPath = "resources/sinErrores/";
 
-    //TODO: el tipo de esta variable init tiene que ser la clase que tiene el main
+    //TO-DO: el tipo de esta variable init tiene que ser la clase que tiene el main
     private static final Main init = null;
    
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
